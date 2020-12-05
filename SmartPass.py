6c9fe855ded9a33824d44ddb3fa1e6a8f51a31e7
zhassan2 <zhassan2students.kennesaw.edu>
2020-12-04 18:40:35 -0800

    Removed the copy function and replaced it with a 'clear text' function. This was a better decision because users might want to quickly clear the generated password for privacy reasons or because it may not be the right one for them.

--- SmartPass.py
+++ SmartPass.py
@@ -45,9 +45,9 @@ def generate():
 
 
 # Function for copying password to clipboard 
-def copy1(): 
-	random_password = entry.get() 
-	pyperclip.copy(random_password) 
+def clear_text(): 
+	entry.delete(0, END)
+
 
 
 # Main Function 
@@ -74,8 +74,8 @@ c_label.grid(row=1)
 # create Buttons Copy which will copy 
 # password to clipboard and Generate 
 # which will generate the password 
-copy_button = Button(root, text="Copy", command=copy1) 
-copy_button.grid(row=0, column=2) 
+clear_button = Button(root, text="Clear", command=clear_text) 
+clear_button.grid(row=0, column=2) 
 generate_button = Button(root, text="Generate", command=generate) 
 generate_button.grid(row=0, column=3) 
 
